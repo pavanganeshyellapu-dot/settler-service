797eb909177c9ba9b839c36623daeff96753b7a3
Pavan <pavanganeshyellapu@gmail.com>
2025-10-25 22:00:28 +0530

    Resolved merge conflict: removed logs/settler-service.log

--- src/main/java/com/settler/domain/groups/dto/CreateGroupRequest.java
+++ src/main/java/com/settler/domain/groups/dto/CreateGroupRequest.java
@@ -12,6 +12,7 @@ public class CreateGroupRequest {
     @NotBlank(message = "name is required")
     private String name;
 
+
     @NotBlank(message = "currencyCode is required")
     // optional: validate ISO currency with regex or enum
     private String currencyCode;
